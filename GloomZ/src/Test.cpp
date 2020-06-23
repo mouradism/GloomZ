0efb3870b94d78ebdd3c8d5eaa90dc2a3dfc1541
mourad <mourad.smail@gmail.com>
2020-06-23 20:54:59 +0100

    mj1

---
+++ GloomZ/src/Test.cpp
@@ -0,0 +1,8 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace GloomZ {
+	void Print() {
+		printf( "GloomZ engine Starting! \n" );
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,9 @@
+namespace GloomZ {
+	__declspec(dllimport) void Print();
+}
+
+void main() {
+	GloomZ::Print();
+
+
+}
